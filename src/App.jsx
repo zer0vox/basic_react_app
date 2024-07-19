b9cb327c188ee53f1bb01bd8419b7901f52be0f5
Sumip Chaudhary <sumip780@gmail.com>
2024-07-19 09:05:47 +0545

    added dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 //import Car from "./Car";
-import Contact from "./contact";
-//import Survey from "./survey";
+//import Contact from "./contact";
+import Survey from "./survey";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-date-picker/dist/DatePicker.css";
+import "react-calendar/dist/Calendar.css";
 import { StateHook } from "./StateHook";
 const App = () => {
   return (
     <>
-      <StateHook />
+      <Survey />
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-date-picker/dist/DatePicker.css";
+import "react-calendar/dist/Calendar.css";
+import { StateHook } from "./StateHook";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
